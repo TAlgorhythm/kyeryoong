5fc9972307943a92b7e8340e533b08b3940bc604
youngwoo_kim4 <youngwoo_kim4@tmax.co.kr>
2024-04-08 18:51:29 +0900

    프로그래멋 12909 수정

--- Stack & Queue/P12909_올바른 괄호.js
+++ Stack & Queue/P12909_올바른 괄호.js
@@ -1,6 +1,7 @@
 // P12909. 올바른 괄호
 // [프로그래머스] https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
+// 스택의 마지막 원소를 반환하는 함수
 function lastIndex(stack) {
   return stack.slice(-1)[0];
 }
