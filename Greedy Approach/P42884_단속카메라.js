29ed5b82da7d72bfb4c3965f23c786aaf1230be9
kyeryoong <kyeryoong@gmail.com>
2024-05-28 01:10:44 +0900

    프로그래머스 42884 - 단속카메라 - 수정

--- Greedy Approach/P42884_단속카메라.js
+++ Greedy Approach/P42884_단속카메라.js
@@ -1,3 +1,6 @@
+// P42884. 단속카메라
+// [프로그래머스] https://school.programmers.co.kr/learn/courses/30/lessons/42884
+
 function solution(routes) {
   routes.sort((a, b) => a[1] - b[1]);
 
@@ -18,3 +21,12 @@ function solution(routes) {
 
   return answer;
 }
+
+console.log(
+  solution([
+    [-20, -15],
+    [-14, -5],
+    [-18, -13],
+    [-5, -3],
+  ])
+);
