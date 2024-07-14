677bf09331cfa73dcb7a9f21b0c2fe6494005985
kyeryoong <kyeryoong@gmail.com>
2024-07-15 01:04:38 +0900

    프로그래머스 70129 - 이진 변환 반복하기 - 수정

--- Implementation/P70129_이진 변환 반복하기.js
+++ Implementation/P70129_이진 변환 반복하기.js
@@ -1,21 +1,6 @@
 // P70129. 이진 변환 반복하기
 // [프로그래머스] https://school.programmers.co.kr/learn/courses/30/lessons/70129
 
-// 십진수를 이진수로 변환해주는 함수
-function toBinary(number) {
-  var result = [];
-
-  while (number > 0) {
-    const quotient = parseInt(number / 2);
-    const remainder = number % 2;
-
-    result.unshift(remainder);
-    number = quotient;
-  }
-
-  return Number(result.join(""));
-}
-
 function solution(s) {
   var count = 0; // 변환의 횟수
   var deleted = 0; // 변환 과정에서 제거된 모든 0의 개수
@@ -29,7 +14,7 @@ function solution(s) {
     deleted = deleted + String(s).length - zerosDeleted.length;
 
     // 0 제거 후 길이를 이진 변환 수행
-    s = toBinary(zerosDeleted.length);
+    s = Number(zerosDeleted.length.toString(2));
   }
 
   return [count, deleted];
