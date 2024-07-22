c9dbcbe7a09c6bce80eea2515d0e4a7ba5725f0c
kyeryoong <kyeryoong@gmail.com>
2024-07-23 01:13:03 +0900

    백준 1463 - 1로 만들기 - 수정

--- Dynamic Programming/B1463_1로 만들기.js
+++ Dynamic Programming/B1463_1로 만들기.js
@@ -13,12 +13,15 @@ function solution(n) {
   const dp = Array(n + 1).fill(0);
 
   for (let i = 2; i < n + 1; i++) {
+    // 현재 숫자에서 1을 빼는 경우
     dp[i] = dp[i - 1] + 1;
 
+    // 현재 숫자가 3으로 나누어 떨어지는 경우
     if (i % 3 === 0) {
       dp[i] = Math.min(dp[i], dp[i / 3] + 1);
     }
 
+    // 현재 숫자가 2로 나누어 떨어지는 경우
     if (i % 2 === 0) {
       dp[i] = Math.min(dp[i], dp[i / 2] + 1);
     }
